d36b2431263295600cea80049e8d5d4486dc1c74
marine1213 <cncuong93@gmail.com>
2020-08-13 16:20:52 +0700

    Update sidebar - add project listing
    - Update side bar
    - Fix hideonsmall of statistic table
    - Add project option in form

--- public/lib/logScript.js
+++ public/lib/logScript.js
@@ -1,4 +1,5 @@
 // description="debug"
   logF=(functionObj,paramObj)=>{console.log(`${functionObj.name}(${JSON.stringify(paramObj)})`); functionObj(paramObj);};
   logE=(information,error)=>{if(information)console.log(information);if(error)console.error(error)}
+  logO=(information,error)=>{logE(information,error);return information;}
 //=================================================
